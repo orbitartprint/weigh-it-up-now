cc382e5fab7b74459f89dcaeb6fca2222bbc1539
orbitartprint <orbitartprint@gmail.com>
2025-07-12 10:00:34 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 WeightVs.com. All rights reserved.</p>
+          <p>&copy; 2025 WeightVs.com. All rights reserved.</p>
         </div>
       </div>
     </footer>
